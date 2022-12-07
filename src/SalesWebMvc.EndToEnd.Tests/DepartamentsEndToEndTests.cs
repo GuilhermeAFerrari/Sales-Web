fa296c9bd63d0f4789f6601ecf60a3db9eab6e66
Guilherme Ferrari <guile.ferrari@hotmail.com>
2022-12-06 21:02:55 -0300

    test: add new test end to end for new departament

---
+++ src/SalesWebMvc.EndToEnd.Tests/DepartamentsEndToEndTests.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Reflection;
+
+namespace SalesWebMvc.EndToEnd.Tests;
+
+public class DepartamentsEndToEndTests
+{
+    [Fact]
+    public void LoadDepartaments()
+    {
+        // Arrange
+        IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+        // Act
+        driver.Navigate().GoToUrl("https://localhost:7168/Departaments");
+        driver.FindElement(By.LinkText("Create New")).Click();
+        driver.FindElement(By.Id("Name")).SendKeys("Departament Tests Selenium Web Driver");
+        driver.FindElement(By.Id("Create")).Click();
+
+        // Assert
+        Assert.Contains("Index - SalesWebMvc", driver.Title);
+        Assert.Contains("Departament Tests Selenium Web Driver", driver.PageSource);
+    }
+}
